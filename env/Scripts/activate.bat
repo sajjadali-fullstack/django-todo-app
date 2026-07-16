49e4e8751ca3f971472efcc5147e836c39b00a3d
Sajjad Ali <yoursajju61@gmail.com>
2026-07-16 16:56:38 +0530

    feat: Deployment

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Git-Hub\To-Do App\ToDoList\env
+set VIRTUAL_ENV=C:\Git-Hub\To-Do App\todoProject\env
 
 if not defined PROMPT set PROMPT=$P$G
 
